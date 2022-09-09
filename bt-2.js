05eee430fd2038ba78a963243f8ffa536f22fef0
Tri-Nguyen-C0822g1 <tringuyen10396@gmail.com>
2022-09-09 11:05:28 +0700

    add bt-2

---
+++ bt-2.js
@@ -0,0 +1,10 @@
+
+let inputWidth;
+let inputHight;
+inputWidth = prompt("Enter the Width");
+inputHight = prompt("Enter the Hight");
+let width = parseInt(inputWidth);
+let wight = parseInt(inputHight);
+let area = width*wight;
+document.write(" The Area is =" + area);
+
